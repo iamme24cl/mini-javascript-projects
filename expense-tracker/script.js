63a49ce57dfcbff9233cf48623ef4140d31c73b2
Chandra Lama <iamme24cl@gmail.com>
2021-03-15 20:27:05 -0700

    implemented adding and deleting transactions

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -15,6 +15,33 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+
+    text.value = '';
+    amount.value= '';
+  }
+}
+
+// Generate random ID 
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
   // Get sign
@@ -26,7 +53,8 @@ function addTransactionDOM(transaction) {
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
   item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn">x</button>
+  ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
+  </span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
   `;
 
   list.appendChild(item);
@@ -53,6 +81,14 @@ function updateValues() {
   money_minus.innerText = `$${expense}`;
 }
 
+// Remove Transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+
+  console.log(transactions);
+  init();
+}
+
 // Init app
 function init() {
   list.innerHTML = '';
@@ -62,3 +98,5 @@ function init() {
 }
 
 init();
+
+form.addEventListener('submit', addTransaction);
